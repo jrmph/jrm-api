080604bd614151c42e7e9721b960a9be04295816
lanceajiro <142672718+lanceajiro@users.noreply.github.com>
2025-10-26 09:09:12 +0000

    Ey

--- settings.js
+++ settings.js
@@ -3,5 +3,15 @@ module.exports = {
     description: 'This interactive interface allows you to explore and test our comprehensive collection of API endpoints in real-time.',
     icon: '/docs/image/icon.png',
     author: 'ShinDesu',
-    telegram: 'https://t.me/+AQO22J2q6KBlNWM1'
+    telegram: 'https://t.me/+AQO22J2q6KBlNWM1',
+    notification: [ 
+        {
+            title: 'New API Added',
+            message: 'Blue Achieve and Loli API have been added to the documentation.',
+        },
+        {
+            title: 'System Update',
+            message: 'The API documentation system has been updated to version 0.0.2'
+        }
+    ]
 };
